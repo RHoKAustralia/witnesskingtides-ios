c736c724c30103e2864fadd5f54e73aa5d3c7f85
Andrew Spinks <aspinks@dius.com.au>
2014-01-29 16:12:50 +1100

    More code signing palaver.

--- travis/add-key.sh
+++ travis/add-key.sh
@@ -1,6 +1,6 @@
 security create-keychain -p travis ios-build.keychain
-# security import ./scripts/travis/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-# security import ./scripts/travis/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./Provisioning/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./Provisioning/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./Provisioning/kt-dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $CERT_PASS -T /usr/bin/codesign
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./Provisioning/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
