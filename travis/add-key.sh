33f233ee1eb9db5431c6209ca548fb598ed00765
Andrew Spinks <andrewspinks@gmail.com>
2014-06-07 16:08:58 +1000

    Try to fix travis-ci build

--- travis/add-key.sh
+++ travis/add-key.sh
@@ -1,7 +1,9 @@
 security create-keychain -p travis ios-build.keychain
+security default-keychain -s ios-build.keychain
+security unlock-keychain -p travis ios-build.keychain
+security -v set-keychain-settings -lut 86400 ios-build.keychain
 security import ./Provisioning/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./Provisioning/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./Provisioning/kt-dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $CERT_PASS -T /usr/bin/codesign
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./Provisioning/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
-
